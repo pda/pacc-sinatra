c5cecc5c5a3312213adb2a4b66247c579edad0d5
Paul Annesley <paul@annesley.cc>
2009-04-07 22:05:30 +1000

    CouchDB client supports GET, PUT and DELETE.

--- pacc/couch.rb
+++ pacc/couch.rb
@@ -5,13 +5,51 @@ module Pacc
 
 # A simple CouchDB interface
 class Couch
+
   def initialize(baseurl)
     @baseurl = baseurl
     @client = HTTPClient.new
   end
-  def view(path)
-    CouchView.new(JSON.parse(@client.get_content("#{@baseurl}/_view/#{path}")))
+
+  def view(path, params = {})
+    CouchView.new(decode(@client.get_content(url("_view/#{path}", params))))
   end
+
+  def get(id)
+    decode(@client.get_content(url(id)))
+  end
+
+  def put(id, data)
+    url = url(id)
+    r = @client.put(url, encode(data))
+    # TODO: better handling - actually, rewrite this whole class
+	unless r.status_code == 201
+      puts r.inspect
+      raise "HTTP error #{r.status_code} #{r.header.reason_phrase} from PUT #{url}" unless r.status_code == 200
+	end
+  end
+
+  def delete(id, revision)
+    @client.delete(url(id, :rev => revision))
+  end
+
+  private
+
+  def url(fragment, params = {})
+    url = "#{@baseurl}/#{fragment}"
+    # TODO: get rid of this dodginess
+    url += '?' + params.map{ |k,v| "%s=%s" % [k,v] }.join('&') unless params.empty?
+    url
+  end
+
+  def encode(data)
+    JSON.generate(data)
+  end
+
+  def decode(json)
+    JSON.parse(json)
+  end
+
 end
 
 # a CouchDB view
