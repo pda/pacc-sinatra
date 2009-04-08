226d4d9cb28599768da253898be9bb9af6459fd3
Paul Annesley <paul@annesley.cc>
2009-04-08 21:27:05 +1000

    Comments are imported from old SQLite3 database.

--- import_sqlite3.rb
+++ import_sqlite3.rb
@@ -6,20 +6,24 @@ require 'sqlite3'
 require 'time'
 
 couch = Pacc::Couch.new('http://localhost:5984/pacc')
+
 sqlite = SQLite3::Database.new('tmp/pdawebsite.sqlite3')
+sqlite.results_as_hash = true
 
 # delete existing posts from CouchDB
 couch.view('blog/posts').rows.each do |row|
-  puts "deleting #{row['_id']} - #{row['title']}"
+  puts "Deleting blogpost #{row['_id']} - #{row['title']}"
   couch.delete row['_id'], row['_rev']
 end
 
-sqlite.results_as_hash = true
+# import blog posts
+post_id_map = {}
 sqlite.execute 'SELECT * FROM blog_blogpost ORDER BY timecreated' do |row|
   created = DateTime.parse(row['timecreated'])
   modified = DateTime.parse(row['timemodified'])
   id = "%04d-%02d-%s" % [ created.year, created.month, row['slug'] ]
-  puts "importing #{id} - #{row['title']}"
+  post_id_map[row['id']] = id
+  puts "Importing blogpost #{id} - #{row['title']}"
   document = {
     :type => 'blogpost',
     :slug => row['slug'],
@@ -27,7 +31,31 @@ sqlite.execute 'SELECT * FROM blog_blogpost ORDER BY timecreated' do |row|
     :timecreated => created.strftime('%Y/%m/%d %H:%M:%S %z'),
     :timemodified => modified.strftime('%Y/%m/%d %H:%M:%S %z'),
     :content => row['content'],
-	:commentable => row['allowcomments'],
+    :commentable => row['allowcomments'],
   }
   couch.put(id, document)
+end
+
+# delete existing comments from CouchDB
+couch.view('blog/allcomments').rows.each do |row|
+  puts "Deleting comment #{row['_id']} by #{row['authorname']} from #{row['timecreated']}"
+  couch.delete row['_id'], row['_rev']
+end
+
+sqlite.execute 'SELECT * FROM blog_blogpostcomment WHERE approved = 1 ORDER BY id' do |row|
+  created = DateTime.parse(row['timecreated'])
+  row['post_id'] = post_id_map[row['blogpost_id']]
+  puts "Importing comment #%s re %s by %s on %s" % row.values_at('id','post_id','authorname','timecreated')
+  document = {
+    :type => 'blogpostcomment',
+    :blogpost_id => row['post_id'],
+    :timecreated => created.strftime('%Y/%m/%d %H:%M:%S %z'),
+    :authorname => row['authorname'],
+    :authoremail => row['authoremail'],
+    :authorurl => row['authorurl'],
+    :authorip => row['authorip'],
+    :authoruseragent => row['authoruseragent'],
+    :content => row['content'],
+  }
+  couch.post(document)
 end
